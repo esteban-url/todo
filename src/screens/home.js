e57d8003c2d085a338a3be08c38bb0bbf7151b3c
Esteban Rojas <estebanjrojas@gmail.com>
2020-10-15 11:54:13 -0600

    set up tailwind and re-structure app

---
+++ src/screens/home.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Button from "../components/button"
+
+const Home = () => {
+  return (
+    <>
+      <Button>Hola</Button>
+    </>
+  )
+}
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
