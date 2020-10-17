f5ac9deb2d1573f60ac91143d1c70108c6394d8f
Esteban Rojas <estebanjrojas@gmail.com>
2020-10-17 16:27:54 -0600

    adding redirects, making complete icosn bigger

--- src/components/todo.js
+++ src/components/todo.js
@@ -23,7 +23,7 @@ const Todo = ({ todo, onDelete, onEdit, ...rest }) => {
         onClick={handleComplete}
       >
         <svg
-          className={`w-4 h-4`}
+          className={`w-6 h-6`}
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
